3f25ea1c97bae5c13ff33ae40106701911c590ee
Rick Ellis <riccello@yahoo.com>
2018-02-23 15:17:09 -0500

    Create stage.cs

---
+++ stage.cs
@@ -0,0 +1,25 @@
+var LinearStage = function (options) {
+
+    var name = options.name;
+    
+    var pins = {
+      ms1: options.pins.ms1,
+      ms2: options.pins.ms2,
+      ms3: options.pins.ms3,
+      dir: options.pins.dir,
+      pwm: options.pins.pwm
+    };
+    
+    var msmm = options.microstepspermm;
+    
+    return new function () {
+        
+        var self = this;
+        
+        self.movesteps = function (steps) {
+            
+        };
+
+        return self;
+    };
+};
