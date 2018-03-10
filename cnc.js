ef48f5b5539bffebc87c9a38677d794dc6e1fbcb
Rick Ellis <riccello@yahoo.com>
2018-03-09 19:21:25 -0500

    Update cnc.js

--- cnc.js
+++ cnc.js
@@ -18,7 +18,8 @@ var cncjs = cncjs || new (function () {
     var m3dqueue = new Array();
     var m3dexecuting = false;
     
-    var _m3dws = new WebSocket('ws://' + window.location.hostname + ':81/');
+    var _websock;
+    var _websockconnected = false;;
     
     var _nextM3dCommand = function(){
         if(m3dqueue.length<1){
@@ -92,6 +93,10 @@ var cncjs = cncjs || new (function () {
         
         //bind keyboard shortcuts
         $(document).keydown(function (e) {
+            
+            if(!e.ctrlKey){ //only respond to keyboard CNC control shortcuts when CTRL key is held down
+                return;
+            }
             
             if(keymap[e.keyCode]){
                 return;
@@ -178,16 +183,52 @@ var cncjs = cncjs || new (function () {
         
     };
     
-    
+    var _connect = function() {
+      return new Promise((resolve, reject) => {
+        if(_websockconnected){
+            resolve(_websock);
+        }else{
+            if(!_websock){
+                _websock = new WebSocket('ws://' + window.location.hostname + ':81/');    
+            }
+            else {
+                _websock.removeAllListeners();
+                _websock.open('ws://' + window.location.hostname + ':81/');    
+            }
+            
+            _websock.onopen = function(e){ 
+                _websockconnected = true;
+                resolve(_websock);
+             };
+            
+            _websock.onmessage = function(data,flags,number){
+                _axis.X.socketmessage(data, flags, number);
+                _axis.Y.socketmessage(data, flags, number);
+                _axis.Z.socketmessage(data, flags, number);
+            };
+            
+            _websock.onclose = function(e){ 
+                _websockconnected = false;
+                console.log('Websocket closed');
+             };
+            
+            _websock.onerror = function(e){ 
+                _websockconnected = false;
+                console.err(arguments);
+             };
+        }
+      });
+    }
 
+   
 
     return new function () {
 
         var _this = this; //save the instance reference because 'this' will always change
 
         _this.axis = _axis;
-        
         _this.move3d = _move3d;
+        _this.connect = _connect;
 
         _init();
 
@@ -197,58 +238,58 @@ var cncjs = cncjs || new (function () {
 })();
 
 
-function toggle(e) {
-    if (e.innerHTML == 'Off') {
-        cncjs.writePin(e.getAttribute('pin'), 1);
-        e.innerHTML = 'On';
-    }
-    else {
-        cncjs.writePin(e.getAttribute('pin'), 0);
-        e.innerHTML = 'Off';
-    }
-}
+// function toggle(e) {
+//     if (e.innerHTML == 'Off') {
+//         cncjs.writePin(e.getAttribute('pin'), 1);
+//         e.innerHTML = 'On';
+//     }
+//     else {
+//         cncjs.writePin(e.getAttribute('pin'), 0);
+//         e.innerHTML = 'Off';
+//     }
+// }
 
 
-function stepPWM(e) {
-    var dirpin = $('#movedirpin').val().padStart(2, "0");
-    var pwmpin = $('#movepwmpin').val().padStart(2, "0");
+// function stepPWM(e) {
+//     var dirpin = $('#movedirpin').val().padStart(2, "0");
+//     var pwmpin = $('#movepwmpin').val().padStart(2, "0");
 
-    var ondur = $('#moveondur').val().padStart(3, "0");
-    var offdur = $('#moveoffdur').val().padStart(3, "0");
+//     var ondur = $('#moveondur').val().padStart(3, "0");
+//     var offdur = $('#moveoffdur').val().padStart(3, "0");
 
-    var steps = parseInt($('#movesteps').val());
-    if (!(steps != 0)) {
-        return;
-    }
-    var dir = 1;
-    if (steps < 0) {
-        steps = Math.abs(steps);
-        dir = 0;
-    }
-    websock.send('exe.pin.' + pwmpin + '.0');  //disengage
-    websock.send('exe.pin.' + dirpin + '.' + dir);  //dir
-    websock.send('mov.pin.' + pwmpin + '.' + ondur + '.' + offdur + '.' + steps);
-}
-function flashText(opt, cycle) {
-    cycle = cycle || 1;
+//     var steps = parseInt($('#movesteps').val());
+//     if (!(steps != 0)) {
+//         return;
+//     }
+//     var dir = 1;
+//     if (steps < 0) {
+//         steps = Math.abs(steps);
+//         dir = 0;
+//     }
+//     websock.send('exe.pin.' + pwmpin + '.0');  //disengage
+//     websock.send('exe.pin.' + dirpin + '.' + dir);  //dir
+//     websock.send('mov.pin.' + pwmpin + '.' + ondur + '.' + offdur + '.' + steps);
+// }
+// function flashText(opt, cycle) {
+//     cycle = cycle || 1;
 
-    var curcol = opt.fgcol;
+//     var curcol = opt.fgcol;
 
-    if (cycle % 2 === 0) { //invert colors on even cycles
-        curcol = opt.bgcol;
-    }
+//     if (cycle % 2 === 0) { //invert colors on even cycles
+//         curcol = opt.bgcol;
+//     }
 
-    websock.send('txt.dsp.' + curcol +
-        '.' + opt.size +
-        '.' + opt.align +
-        '.' + opt.x.toString().padStart(3, '0') +
-        '.' + opt.y.toString().padStart(3, '0') +
-        '.' + opt.text);
+//     websock.send('txt.dsp.' + curcol +
+//         '.' + opt.size +
+//         '.' + opt.align +
+//         '.' + opt.x.toString().padStart(3, '0') +
+//         '.' + opt.y.toString().padStart(3, '0') +
+//         '.' + opt.text);
 
-    if (cycle < opt.num * 2) {
-        setTimeout(flashText, opt.delayms, opt, cycle + 1);
-    }
-    else {
-        websock.send('clr.dsp');
-    }
-}
+//     if (cycle < opt.num * 2) {
+//         setTimeout(flashText, opt.delayms, opt, cycle + 1);
+//     }
+//     else {
+//         websock.send('clr.dsp');
+//     }
+// }
