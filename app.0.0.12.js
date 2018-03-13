996440fb641eebdeb5582020c4f1250a49f03cd0
Rick Ellis <riccello@yahoo.com>
2018-03-13 15:37:22 -0400

    Update and rename app.0.0.11.js to app.0.0.12.js

---
+++ app.0.0.12.js
@@ -0,0 +1,71 @@
+var _requiredScripts = [];
+var _mainScript = '';
+
+var _cncsocket = null;
+var _createWebSocket = function(){
+    _cncsocket = new WebSocket('ws://' + window.location.hostname + ':81/');
+};
+
+var _codeversionmessageparser = function(evt){
+  if(evt.data.indexOf('memdata:')>=0){
+    var kv = evt.data.split(':')[1];
+    var t = kv.split('=');
+    var addr = t[0];
+    if(addr=='0'){
+      var gittag = t[1];
+      $('#gittaginput').val(gittag);
+      //git version
+      var files = ['etag.js', 'linearstage.js', 'rotarystage.js']; //main file last
+      files.forEach(function(filename){
+        _loadScript('https://cdn.rawgit.com/nycdude777/cncjs/' + gittag + '/' + filename, true);
+      });
+      _mainScript = 'https://cdn.rawgit.com/nycdude777/cncjs/' + gittag + '/cnc.js'
+      _cncsocket.removeEventListener('message', _codeversionmessageparser);
+    }
+  }
+};
+
+var _codeversionconnectionhandler = function(){
+  _cncsocket.removeEventListener('open', _codeversionconnectionhandler);
+  _cncsocket.addEventListener('message', _codeversionmessageparser);
+  ws.send('mem.get.000.007');
+};
+
+function getversion(){
+  _createWebSocket();
+  _cncsocket.addEventListener('open', _codeversionconnectionhandler);
+}
+
+function setversion() { 
+  var value = $('#gittaginput').val(); 
+  _wsconnect().then(function(ws){
+    ws.send('mem.set.000.007.'+value); 
+  });
+}
+
+var _loadScript = function(url, require)  { 
+  var script = document.createElement('script');
+  script.src = url;
+  if(require){
+    script.setAttribute('onload', '_scriptLoaded(this)');
+    _requiredScripts.push(script);  
+  }
+  document.head.appendChild(script); 
+};
+
+var _scriptLoaded = function (tag) {
+    var numloaded = 0;
+    var src = tag.getAttribute('src');
+    _requiredScripts.forEach(function(scr) {
+      if(scr.src === src){
+        scr.loaded = 1;
+      }
+      if(scr.loaded){
+        numloaded++;
+      }
+    });
+    if(numloaded===_requiredScripts.length){
+      _loadScript(_mainScript, false);
+    }
+};
+
