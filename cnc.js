3047d21bbc1c28df5fa20ea11289ee69bb4497be
Rick Ellis <riccello@yahoo.com>
2018-03-15 22:33:22 -0400

    Update cnc.js

--- cnc.js
+++ cnc.js
@@ -44,7 +44,7 @@ var cnc = cnc || new (function () {
             pins: { ms1: 0, ms2: 1, ms3: 2, dir: 3, pwm: 4 } }),
         Y: new LinearStage({ name: "Y axis", resolution: 5000,
             pins: { ms1: 8, ms2: 9, ms3: 10, dir: 11, pwm: 12 } }),
-        Z: new LinearStage({ name: "Z axis", resolution: 4000, 
+        Z: new LinearStage({ name: "Z axis", resolution: 2422, 
             pins: { ms1: 24, ms2: 25, ms3: 26, dir: 27, pwm: 28 } })
     };
 
