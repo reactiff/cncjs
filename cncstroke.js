4440041ba263c52ba3a15aaa7e42dbda31668f98
Rick Ellis <riccello@yahoo.com>
2018-03-17 20:33:15 -0400

    Create cncstroke.js

---
+++ cncstroke.js
@@ -0,0 +1,64 @@
+var CncStroke = function () {
+
+    var _this;
+
+    var _args = [].slice.apply(arguments).slice(0);
+
+    if (_args.length % 2 != 0) throw 'Coordinate parity error.  Stroke requires an even numberar of arguments, i.e. x1, y1, x2, y2 et.'
+
+    const _start = new cnc.GliphPoint();
+    const _points = [];
+
+    return new function () {
+        _this = this;
+
+        for (var i = 0; i < _args.length; i+=2) {
+            if (i === 0) {
+                _start.x = _args[i];
+                _start.y = _args[i + 1];
+            }
+            else {
+                _points.push(new cnc.GliphPoint(_args[i], _args[i+1]));
+            }
+        }
+
+        _this.setparent = (gliph) => {
+            _this.parentgliph = gliph;
+        };
+
+        _this.startpoint = function () {
+            return _start;
+        };
+
+        _this.startpos = function () {
+            return new Vector(
+                _start.x * _this.parentgliph.strokewidth,
+                _start.y * _this.parentgliph.strokewidth
+            );
+        };
+
+        _this.getpoints = () => { return _points; };
+
+        _this.allpoints = () => {
+            var allpoints = [];
+            allpoints.push(_start);
+            _points.forEach(function (p) { allpoints.push(p);});
+            return allpoints;
+        };
+
+        _this.getmoves = () => {
+            var vectors = [];
+            var lastpoint = _start;
+            _points.forEach(function (p) {
+                vectors.push(new Vector(
+                    (p.x - lastpoint.x) * _this.parentgliph.strokewidth,
+                    (p.y - lastpoint.y) * _this.parentgliph.strokewidth
+                ));
+                lastpoint = p;
+            });
+            return vectors;
+        };
+
+        return _this;
+    };
+};
