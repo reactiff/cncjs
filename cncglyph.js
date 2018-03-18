0f3712dde3e8cb721b80390fb5634d9b95fab76a
Rick Ellis <riccello@yahoo.com>
2018-03-18 10:44:29 -0400

    Update cncglyph.js

--- cncglyph.js
+++ cncglyph.js
@@ -1,23 +1,28 @@
 var CncGlyph = function () {
 
     var _this;
-    
+        
+    var _argchar = arguments[0];
+    var _argstrokes = arguments[1];
+
     var _strokes = [];
-    var _args = [].slice.apply(arguments).slice(0);
-    
+
     return new function () {
         _this = this;
-
-        _this.char = _args[0];
+        
+        _this.char = _argchar;
         _this.strokewidth = 0.2;
-
-        for (var i = 1; i < _args.length; i++) {
-            var stroke = _args[i];
+        
+        for (var i = 0; i < strokes.length; i++) {
+            var stroke = strokes[i];
             stroke.setparent(_this);
             _strokes.push(stroke);
         }
 
         _this.widthinpoints = () => {
+            if (_this.hasOwnProperty('customwidthinpoints')) {
+                return _this.customwidthinpoints;
+            }
             var points = 0;
             _strokes.forEach(function (s) {
                 s.allpoints().forEach(function (p) {
@@ -30,6 +35,9 @@ var CncGlyph = function () {
         };
 
         _this.heightinpoints = () => {
+            if (_this.hasOwnProperty('customheightinpoints')) {
+                return _this.customheightinpoints;
+            }
             var points = 0;
             _strokes.forEach(function (s) {
                 s.allpoints().forEach(function (p) {
