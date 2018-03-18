d81bce439b376373020b33101bbce622a3f58358
Rick Ellis <riccello@yahoo.com>
2018-03-17 23:35:25 -0400

    Update cncfontsimple.js

--- cncfontsimple.js
+++ cncfontsimple.js
@@ -1,4 +1,4 @@
-var CncFontSimple = function () {
+var CncFontSimple = cnc.FontSimple = function () {
 
     var _this;
     
@@ -6,28 +6,28 @@ var CncFontSimple = function () {
 
         _this = this;
 
-        _this.gliphs = {};
+        _this.glyphs = {};
         
-        _this.gliphs['A'] = new cnc.Gliph('A',
+        _this.glyphs['A'] = new cnc.Glyph('A',
             new Stroke(0, 6, 0, 1, 1, 0, 3, 0, 4, 1, 4, 6),
             new Stroke(0, 3, 4, 3)
         );
 
-        _this.gliphs['B'] = new cnc.Gliph('B',
+        _this.glyphs['B'] = new cnc.Glyph('B',
             new Stroke(1, 3, 3, 3, 4, 4, 4, 5, 3, 6, 0, 6, 0, 0, 3, 0, 4, 1, 4, 2)
         );
 
-        _this.gliphs['C'] = new cnc.Gliph('C',
+        _this.glyphs['C'] = new cnc.Glyph('C',
             new Stroke(4, 1, 3, 0, 1, 0, 0, 1, 0, 5, 1, 6, 3, 6, 4, 5)
         );
 
-        _this.getgliph = (c) => {
+        _this.getglyph = (c) => {
             var g;
             if (c === ' ') {
-                g = new cnc.Gliph();
+                g = new cnc.Glyph();
                 return ;
             }
-            g = _this.gliphs[c];
+            g = _this.glyphs[c];
             return g;
         };
 
