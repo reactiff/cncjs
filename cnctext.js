0261af64b218634cc55b11384a41a55921f5d7f4
Rick Ellis <riccello@yahoo.com>
2018-03-18 00:22:59 -0400

    Update cnctext.js

--- cnctext.js
+++ cnctext.js
@@ -9,12 +9,12 @@ var CncText = function (text, font, size) {
     return new function () {
         _this = this;
 
-        _this.getgliphs = function () {
-            var gliphs = [];
+        _this.getglyphs = function () {
+            var glyphs = [];
             for (var i = 0; i < _text.length; i++) {
-                gliphs.push(_font.getgliph(_text[i]));
+                glyphs.push(_font.getglyph(_text[i]));
             }
-            return gliphs;
+            return glyphs;
         };
 
         return _this;
