e992df8e97ed729fa387b8b6a3b6a380f8a19b98
Rick Ellis <riccello@yahoo.com>
2018-03-18 01:03:37 -0400

    Create cnc_wscomm.js

---
+++ cnc_wscomm.js
@@ -0,0 +1,33 @@
+var CncWSCommHandleMessage = function(evt, flags, number) {
+  console.log('[MSG] ' + evt.data);
+  cnc.notify(evt.data);
+  if (evt.data == 'm3d.ok') {
+      cnc.executeNextCommand();
+  }
+};
+    
+var CncAwaitWSConnection = function(resolve){
+    if(_cncsocket.readyState===1){
+        resolve(_cncsocket);
+        return;
+    }
+
+    if(_cncsocket.readyState===2) { //closing
+        _cncsocket.close();
+    }
+
+    if(_cncsocket.readyState===3) { //closed
+        //create a new socket
+        _cncsocket = _createWebSocket();
+        _cncsocket.addEventListener("message", CncWSCommHandleMessage);
+    }
+
+    //if we got here, the socket is opening
+    setTimeout(CncAwaitWSConnection, 50, resolve);
+};
+    
+var CncWSConnect = function () {
+    return new Promise((resolve, reject) => {
+        CncAwaitWSConnection(resolve);
+    });
+};
