ac4c551fdc4ab5d45bb17b2b0aa5bc7adfa14400
Rick Ellis <riccello@yahoo.com>
2018-03-18 00:32:48 -0400

    Create cnc_axes.js

---
+++ cnc_axes.js
@@ -0,0 +1,9 @@
+var Axes = { 
+  
+  X: new LinearStage({ name: "X axis", resolution: 2770, pins: { ms1: 0, ms2: 1, ms3: 2, dir: 3, pwm: 4 } }),
+  
+  Y: new LinearStage({ name: "Y axis", resolution: 5000, pins: { ms1: 8, ms2: 9, ms3: 10, dir: 11, pwm: 12 } }),
+  
+  Z: new LinearStage({ name: "Z axis", resolution: 2422, pins: { ms1: 24, ms2: 25, ms3: 26, dir: 27, pwm: 28 } })
+  
+};
