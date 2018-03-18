ffdd2d6301f971199ae0576bd7423f6c254c7703
Rick Ellis <riccello@yahoo.com>
2018-03-17 20:34:48 -0400

    Create cncgliph.js

---
+++ cncgliph.js
@@ -0,0 +1,60 @@
+var CncGliph = function () {
+
+    var _this;
+    
+    var _args = [].slice.apply(arguments).slice(0);
+
+    var _strokes = [];
+
+    return new function () {
+        _this = this;
+
+
+        _this.char = _args[0];
+        _this.strokewidth = 0.2;
+
+        for (var i = 1; i < _args.length; i++) {
+            var stroke = _args[i];
+            stroke.setparent(_this);
+            _strokes.push(stroke);
+        }
+
+        _this.widthinpoints = () => {
+            var points = 0;
+            _strokes.forEach(function (s) {
+                s.allpoints().forEach(function (p) {
+                    if (p.x + 1 > points) {
+                        points = p.x + 1;
+                    }
+                });
+            });
+            return points;
+        };
+
+        _this.heightinpoints = () => {
+            var points = 0;
+            _strokes.forEach(function (s) {
+                s.allpoints().forEach(function (p) {
+                    if (p.y + 1 > points) {
+                        points = p.y + 1;
+                    }
+                });
+            });
+            return points;
+        };
+
+        _this.width = () => {
+            return _this.widthinpoints() * _this.strokewidth;
+        };
+
+        _this.height = () => {
+            return _this.heightinpoints() * _this.strokewidth;
+        }
+
+        _this.getstrokes = () => {
+            return _strokes;
+        };
+        
+        return _this;
+    };
+};
