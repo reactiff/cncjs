416ca12dc6be9e2b35457e87e016e47aba410150
Rick Ellis <riccello@yahoo.com>
2018-03-17 20:35:17 -0400

    Create cncgliphpoint.js

---
+++ cncgliphpoint.js
@@ -0,0 +1,13 @@
+var CncGliphPoint = function (x, y) {
+
+    var _this;
+    
+    return new function () {
+        _this = this;
+
+        _this.x = x;
+        _this.y = y;
+
+        return _this;
+    };
+};
