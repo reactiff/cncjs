85f2fae10c7497d3db5be6cbd8a12bd6b4769f27
Rick Ellis <riccello@yahoo.com>
2018-03-18 03:17:11 -0400

    Update cncfontsimple.js

--- cncfontsimple.js
+++ cncfontsimple.js
@@ -9,16 +9,16 @@ var CncFontSimple = function () {
         _this.glyphs = {};
         
         _this.glyphs['A'] = new cnc.Glyph('A',
-            new Stroke(0, 6, 0, 1, 1, 0, 3, 0, 4, 1, 4, 6),
-            new Stroke(0, 3, 4, 3)
+            new cnc.Stroke(0, 6, 0, 1, 1, 0, 3, 0, 4, 1, 4, 6),
+            new cnc.Stroke(0, 3, 4, 3)
         );
 
         _this.glyphs['B'] = new cnc.Glyph('B',
-            new Stroke(1, 3, 3, 3, 4, 4, 4, 5, 3, 6, 0, 6, 0, 0, 3, 0, 4, 1, 4, 2)
+            new cnc.Stroke(1, 3, 3, 3, 4, 4, 4, 5, 3, 6, 0, 6, 0, 0, 3, 0, 4, 1, 4, 2)
         );
 
         _this.glyphs['C'] = new cnc.Glyph('C',
-            new Stroke(4, 1, 3, 0, 1, 0, 0, 1, 0, 5, 1, 6, 3, 6, 4, 5)
+            new cnc.Stroke(4, 1, 3, 0, 1, 0, 0, 1, 0, 5, 1, 6, 3, 6, 4, 5)
         );
 
         _this.getglyph = (c) => {
