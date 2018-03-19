99cebce951ce79e62e153b980f5ef87df1943746
Rick Ellis <riccello@yahoo.com>
2018-03-19 10:12:06 -0400

    Create cncdrilling.js

---
+++ cncdrilling.js
@@ -0,0 +1,58 @@
+var CncDrillingMacro = function(stencil) {
+                
+    var drillholes = stencil.getholes();
+
+    //1. foreach diameter
+    var jobsbytoolsize = {};
+    drillholes.forEach(function (hole) {
+        jobsbytoolsize[hole.diameter].execute = function () {
+
+            var _diameter = hole.diameter;
+
+            var holesfordiameter = drillholes.filter(h => h.diameter === _diameter);
+
+            //position the tool over the first hole
+            cnc.movexyto(holesfordiameter[0].origin);
+
+            if (!confirm('Tool change.\n\nPlease install a ' + _diameter + 'mm drill bit before pressing continue...')) {
+                return;
+            }
+
+            if (!confirm('Z axis calibration.\n\nPlease attach Surface Sensors to the platform and the drill bit before continuing...')) {
+                return;
+            }
+
+            cnc.findsurface().then(function () {
+
+                for (var k = 0; k < holesfordiameter.length; k++) {
+
+                    cnc.move(holesfordiameter[k].origin);
+
+                    cnc.drill({
+                        speed: cnc.SPEED.SIXTEENTH,
+                        depth: 3.2,
+                        retract: 3
+                    });
+                }
+                cnc.milestone('diameter' + _diameter);
+            });
+
+        };
+    });
+
+    var diameterkeys = Object.keys(jobsbytoolsize);
+    diameterkeys.forEach(function (diameter, index, arr) {
+        if (index == 0) {
+            //start executing first batch immediately
+            jobsbytoolsize[diameter].execute();
+        }
+        else{
+            cnc.awaitMilestone('diameter' + diameterkeys[index - 1])
+                .then(jobsbytoolsize[diameter].execute);
+        }
+    });
+
+    cnc.milestone('drilling');
+
+    
+};
